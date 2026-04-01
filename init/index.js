07cd5c8df9863cb7d3dd21de53b1de8727b49f0a
Harsh-varshney <harsh.23varshney@gmail.com>
2026-04-01 20:40:11 +0530

    Fix atlas connection and seed data

--- init/index.js
+++ init/index.js
@@ -1,18 +1,24 @@
+require("dotenv").config({ path: "../.env" }); // ADD THIS
+
 const mongoose = require("mongoose");
 const Listing = require("../models/listing.js");
 const initData = require("./data.js");
 
+
+const dbUrl = process.env.ATLASDB_URL; // CHANGE
 main().then(() => {
     console.log("connect successfully");
 }).catch(err => console.log(err));
 
+
 async function main(){
-    await mongoose.connect("mongodb://127.0.0.1:27017/project");
+    // await mongoose.connect("mongodb://127.0.0.1:27017/project");
+     await mongoose.connect(dbUrl); // CHANGE
 }
 
 const initDB = async () =>{
     await Listing.deleteMany({});
-    initData.data = initData.data.map((obj) => ({ ...obj, owner : "653e5b2c69f458a54fdf0bdf"}));
+    initData.data = initData.data.map((obj) => ({ ...obj, owner : "69cd33e90aefd3b064e2a0da"}));
     await Listing.insertMany(initData.data);
     console.log("data was inserted");
 }
